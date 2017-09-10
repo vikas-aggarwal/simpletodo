fad8043a2dac72b7f0f11c7a2184f4c0cad4259c
Vikas Aggarwal <vikas@debian>
2017-09-10 23:08:29 +0530

    removing hostname

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ from simpleTodo import app as application
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
-    httpd = make_server('localhost', 8080, application)
+    httpd = make_server('', 8080, application)
     # Wait for a single request, serve it and quit.
     httpd.serve_forever()
