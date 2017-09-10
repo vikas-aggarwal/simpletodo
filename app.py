dcea689ed3e4d7f1c079089d1ec0849a872d7aba
Vikas Aggarwal <vikas@debian>
2017-09-10 19:33:37 +0530

    Adding app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import os
+from simpleTodo import app as application
+
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+    httpd = make_server('localhost', 8051, application)
+    # Wait for a single request, serve it and quit.
+    httpd.serve_forever()
